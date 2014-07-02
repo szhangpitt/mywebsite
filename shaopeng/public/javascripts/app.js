33f9cc19ea95b6d3350b73598ecc8faa0dd78c44
Shaopeng Zhang <szhangpitt@gmail.com>
2014-07-02 16:44:12 -0400

    Revert "iphone scroll fix try1 and slide-in box effect"
    This reverts commit 76cc1cc77fdd1e7904fcb3d3458878acb792a38d.

--- shaopeng/public/javascripts/app.js
+++ shaopeng/public/javascripts/app.js
@@ -236,7 +236,7 @@ $(document).ready(function (e) {
         else{
             checkAlreadyVisibleStory();
 
-            $(window).on('scroll', function(event) {
+            $(window).scroll(function(event) {
               $(".photography .story, .education .story").each(function(i, el) {
                 var el = $(el);
                 if (el.visible(true)) {
@@ -246,16 +246,6 @@ $(document).ready(function (e) {
               });
               
             });
-
-            document.ontouchmove = function(e) {
-                $(".photography .story, .education .story").each(function(i, el) {
-                var el = $(el);
-                if (el.visible(true)) {
-                  el.addClass("come-in"); 
-                  console.log('come-in', el);
-                } 
-              });
-            };
         }
     }
 
